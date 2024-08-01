d9d271c92947a55f52abebc13ff0685b2d69d6cc
Salma158 <salmasherif856@gmail.com>
2024-08-01 19:20:39 +0300

    Add Base Entity

---
+++ src/main/java/com/qeema/order_management_api/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.qeema.order_management_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+}
