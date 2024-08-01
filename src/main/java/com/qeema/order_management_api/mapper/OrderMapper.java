b7dda198dcd3c66a839de42f8f8e24a7276b698e
Salma158 <salmasherif856@gmail.com>
2024-08-01 22:30:51 +0300

    Add OrderMapper

---
+++ src/main/java/com/qeema/order_management_api/mapper/OrderMapper.java
@@ -0,0 +1,18 @@
+package com.qeema.order_management_api.mapper;
+
+import com.qeema.order_management_api.dto.OrderDto;
+import com.qeema.order_management_api.dto.ProductDto;
+import com.qeema.order_management_api.entity.Order;
+import com.qeema.order_management_api.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OrderMapper {
+
+    OrderMapper MAPPER = Mappers.getMapper(OrderMapper.class);
+
+    OrderDto maptoOrderDto(Order order);
+
+    Order maptoOrder(OrderDto orderDto);
+}
