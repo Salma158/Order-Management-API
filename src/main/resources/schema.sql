f9e56a9e7ca3ac6847c87462317eb417a0e594c9
Salma158 <salmasherif856@gmail.com>
2024-08-01 18:36:43 +0300

    Add Database Schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE  IF NOT EXISTS `product` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `stock` INT NOT NULL,
+    `created_at` date NOT NULL,
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
+
+
+CREATE TABLE  IF NOT EXISTS `order` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `status` VARCHAR(50) DEFAULT 'Pending',
+    `created_at` date NOT NULL,
+    `created_by` varchar(20) NOT NULL,
+    `updated_at` date DEFAULT NULL,
+    `updated_by` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE  IF NOT EXISTS `order_item` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `order_id` INT NOT NULL,
+    `product_id` INT NOT NULL,
+    `quantity` INT NOT NULL,
+    FOREIGN KEY (`order_id`) REFERENCES `order`(`id`),
+    FOREIGN KEY (`product_id`) REFERENCES `product`(`id`),
+    UNIQUE (order_id, product_id)
+);
