d1cca0311f21a1a9d3273db0c617299e11b10649
Salma158 <salmasherif856@gmail.com>
2024-08-01 19:57:40 +0300

    Add Order Entity

--- src/main/java/com/qeema/order_management_api/entity/Order.java
+++ src/main/java/com/qeema/order_management_api/entity/Order.java
@@ -6,23 +6,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
-import java.math.BigDecimal;
-
 @Entity
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product extends BaseEntity{
+public class Order extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
-    private BigDecimal price;
-
-    private Integer stock;
+    private String status;
 }
