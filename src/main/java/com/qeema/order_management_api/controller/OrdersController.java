910061144ddf88a44a0c66b3fbb6b207f314f00d
Salma158 <salmasherif856@gmail.com>
2024-08-02 18:04:35 +0300

    Add fetch orders endpoint

--- src/main/java/com/qeema/order_management_api/controller/OrdersController.java
+++ src/main/java/com/qeema/order_management_api/controller/OrdersController.java
@@ -6,10 +6,9 @@ import com.qeema.order_management_api.service.IProductsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -24,5 +23,13 @@ public class OrdersController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(createdOrder);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> fetchAllOrders(){
+        List<OrderDto> orders = IOrderService.fetchAllOrders();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(orders);
     }
 }
